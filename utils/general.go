5b4b0af289b37ec4553288b7d257f1ff9de18afb
tolik <laktolik9o@gmail.com>
2023-09-28 10:04:44 +0300

    .gitkeep file added in data folder

--- utils/general.go
+++ utils/general.go
@@ -7,7 +7,6 @@ import (
 )
 
 func GenerateUniqueFileName(filename string) string {
-
 	fileName := fmt.Sprintf("(%s)_%s", uuid.New().String()[:8], filename)
 	fullPath := fmt.Sprintf("data/%s", fileName)
 	return fullPath
