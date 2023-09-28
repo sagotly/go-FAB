e10b5399a91ea7c5141129b8a32fdb74dc41b892
tolik <laktolik9o@gmail.com>
2023-09-28 10:15:43 +0300

    linux path fixed

--- utils/general.go
+++ utils/general.go
@@ -7,7 +7,7 @@ import (
 )
 
 func GenerateUniqueFileName(filename string) string {
-	fileName := fmt.Sprintf("(%s)_%s", uuid.New().String()[:8], filename)
+	fileName := fmt.Sprintf("id_%s_%s", uuid.New().String()[:8], filename)
 	fullPath := fmt.Sprintf("data/%s", fileName)
 	return fullPath
 }
